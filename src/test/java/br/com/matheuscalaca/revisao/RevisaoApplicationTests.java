287b97f82bf281d9727488b2c80b33fcdb2cc4ed
matheus calaça <matheusfcalaca@gmail.com>
2023-07-18 20:32:06 -0300

    Meu commit inicial

---
+++ src/test/java/br/com/matheuscalaca/revisao/RevisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheuscalaca.revisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
